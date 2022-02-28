357822e143cb7276bdc289ff77f9fe1c8be666b3
LucasLu412 <72579916+LucasLu412@users.noreply.github.com>
2022-02-27 23:04:05 -0500

    Update Lu_Lucas_ChampionFilter.java

--- grade11 challenge/Lu_Lucas_ChampionFilter.java
+++ grade11 challenge/Lu_Lucas_ChampionFilter.java
@@ -9,69 +9,84 @@ import java.util.*;
 import java.io.*;
 
 public class Lu_Lucas_ChampionFilter {
-    public static void main(String args[]) {
-      try {
-    	FileReader fileReader = new FileReader("champions.json");
-    	BufferedReader output = new BufferedReader(fileReader);
-    	
-    	String lineInfo;
-      
-        //initial armor and hp, the name inside will be replaced with any value in the file
-        //because the hp must be greater than -1 and armor must be lower than 10000000
+    public static void main(String[] args){
+    
+      //initial armor and hp, the name inside will be replaced with any value in the file
+      //because the hp must be greater than -1 and armor must be lower than 10000000
     	double lowestArmor = 10000000;
     	double highestHp = -1;
       
-        //initial all name as empty
+      //initial all name as empty
     	String nameHp = "empty";
     	String nameArmor = "empty";
     	String name = "empty";
-    	
-    	
-    	//use while loop to judge if the system read all the lines inside the file
-    	while ((lineInfo = output.readLine()) != null) {
-           //first split the line with quotation marks
-    	   String[] strSplit = lineInfo.split("\""); 
-          
-          //use length > 2 because if it is not greater than 2, there will be an error of index 1 in the if condition statement
-          if(strSplit.length > 2){
-             //if there occur name, put it in the name so that could be use later
-             if(strSplit[1].equalsIgnoreCase("name")) {
-    	       name = strSplit[3]; 
-    	     }
     	    
-             //if there occur an hp line, split it several times and get the double type value
-    	     if(strSplit[1].equalsIgnoreCase("hp")) {
-    	       String temStr[] = strSplit[2].split(":");
-	       String temStr2[] = temStr[1].split(",");
-               //compare current hp with highest stored before
-    	       if(Double.parseDouble(temStr2[0]) > highestHp) {
-    	         highestHp = Double.parseDouble(temStr2[0]);
-    	         nameHp = name;
-    	       } 	    	
-    	     }
+      //read the file and find the value
+      try {
+    	   FileReader fileReader = new FileReader("champions.json");
+       	BufferedReader output = new BufferedReader(fileReader);    	
+       	String lineInfo;
+          	
+    	   //use while loop to judge if the system read all the lines inside the file
+       	while ((lineInfo = output.readLine()) != null) {
+             //first split the line with quotation marks
+    	       String[] strSplit = lineInfo.split("\""); 
+          
+             //use length > 2 because if it is not greater than 2, there will be an error of index 1 in the if condition statement
+             if(strSplit.length > 2){
+               //if there occur name, put it in the name so that could be use later
+               if(strSplit[1].equalsIgnoreCase("name")) {
+    	     	    name = strSplit[3]; 
+    	         }
+    	    
+               //if there occur an hp line, split it several times and get the double type value
+    	         if(strSplit[1].equalsIgnoreCase("hp")) {
+    	    	     String temStr[] = strSplit[2].split(":");
+	              String temStr2[] = temStr[1].split(",");
+                 //compare current hp with highest stored before
+    	    	     if(Double.parseDouble(temStr2[0]) > highestHp) {
+    	    	       highestHp = Double.parseDouble(temStr2[0]);
+    	    		    nameHp = name;
+    	    	     } 	    	
+    	         }
     	       
-             //if there occur an armor line, split it several times and get the double type value
-    	     if(strSplit[1].equalsIgnoreCase("armor")) {
-    	       String temStr[] = strSplit[2].split(":");
-	       String temStr2[] = temStr[1].split(",");
-               //compare with lowest stored before
-    	       if(Double.parseDouble(temStr2[0]) < lowestArmor) {
-    	         lowestArmor = Double.parseDouble(temStr2[0]);
-    	    	 nameArmor = name;
-    	       } 
-    	     }
-    	  }
-      }
-      System.out.printf("%s has the highest HP: %.2f\n",nameHp, highestHp);
-      System.out.printf("%s has the lowest armor: %.2f\n", nameArmor, lowestArmor);
+               //if there occur an armor line, split it several times and get the double type value
+    	         if(strSplit[1].equalsIgnoreCase("armor")) {
+    	        	  String temStr[] = strSplit[2].split(":");
+	              String temStr2[] = temStr[1].split(",");
+                 //compare with lowest stored before
+    	    	     if(Double.parseDouble(temStr2[0]) < lowestArmor) {
+    	    		    lowestArmor = Double.parseDouble(temStr2[0]);
+    	    		    nameArmor = name;
+    	    	     } 
+    	         }
+    	      }
+         }    
       
-      //close the reader
-      fileReader.close();
-      output.close();
+       	fileReader.close();
+    	   output.close();
       }
       catch(IOException e){
-        System.out.println("Cannot read the file");
+    	   System.out.println("Cannot read the file");
       }
-    
+      
+      //write the file
+      try{
+         File file = new File("MaxChampStats.txt");
+         FileWriter fileWriter = new FileWriter(file);
+         BufferedWriter output = new BufferedWriter(fileWriter);
+         
+         //write the related information to the file
+         output.write(nameHp + " " + highestHp);
+         output.newLine();          
+         output.write(nameArmor + " " + lowestArmor);
+         output.newLine();
+         
+         output.close();
+         fileWriter.close();
+      }
+      catch(IOException e){
+         System.out.println("cannot write the file");
+      }  
     }
 }
